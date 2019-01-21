ae6626f68e7ae8046d80a6704dac553802660670
paulsoumi96 <43988909+paulsoumi96@users.noreply.github.com>
2019-01-21 18:26:04 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-	access_key="${AWS_ACCESS_KEY_ID}"
-	secret_key="${AWS_SECRET_ACCESS_KEY}"
+	access_key="${var.AWS_ACCESS_KEY_ID}"
+	secret_key="${var.AWS_SECRET_ACCESS_KEY}"
 #	access key and secret key from instance detail
  	  region ="ap-southeast-1"
 	}
